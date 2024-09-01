eee51f86988c0bb0304d3c5bcfbf3d7ed12384cb
Conor H <conor.heffron@gmail.com>
2024-09-01 17:27:41 +0100

    Update settings.py
    Use SQL lite for CI build

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        # "ENGINE": "django.db.backends.sqlite3",
-        # "NAME": BASE_DIR / "db.sqlite3",
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "menu_items",
-        "HOST": "127.0.0.1",
-        "PORT":  "3306",
-        "USER": "root",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.mysql",
+        # "NAME": "menu_items",
+        # "HOST": "127.0.0.1",
+        # "PORT":  "3306",
+        # "USER": "root",
     }
 }
 
