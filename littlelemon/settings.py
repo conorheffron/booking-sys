4066425fb8f1605ff75078176df6860795cb2383
Conor H <conor.heffron@gmail.com>
2024-09-04 16:31:04 +0100

    stash-lcl

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = "littlelemon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        # "ENGINE": "django.db.backends.mysql",
-        # "NAME": "reservations",
-        # "HOST": "127.0.0.1",
-        # "PORT":  "3306",
-        # "USER": "root",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "reservations",
+        "HOST": "127.0.0.1",
+        "PORT":  "3306",
+        "USER": "root",
         # "PASSWORD": ""
     }
 }
