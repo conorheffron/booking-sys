b98eeec3113980897204c747a9455ac8de932a9c
Conor H <conor.heffron@gmail.com>
2024-09-05 23:03:45 +0100

    Update urls.py, move static refs

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,6 @@
 """
 littlelemon Views Mapping & Logic
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -12,4 +10,4 @@ urlpatterns = [
     path('reservations/', views.reservations_view, name="reservations"),
     path('bookings/<str:date>/', views.bookings_view, name='bookings_by_date'),
     path('bookings', views.table_view, name='bookings_by_date')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
