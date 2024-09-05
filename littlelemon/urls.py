3a79c9f68ebb2aa9563b702e4c91c4cdec3acedf
Conor H <conor.heffron@gmail.com>
2024-09-05 23:22:20 +0100

    Collect static files
     python3 manage.py collectstatic

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('restaurant.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, })
+]
