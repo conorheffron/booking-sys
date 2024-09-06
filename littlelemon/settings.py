1ab037905deae242326ae20a5e07c11a9a837f6a
Conor H <conor.heffron@gmail.com>
2024-09-06 03:31:08 +0100

    Update static conf

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'restaurant/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'restaurant/static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'restaurant')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'restaurant/static/'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
