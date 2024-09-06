61b7630ff3de5fbc9315ca57d9940c6224b9130f
Conor Heffron <conor.heffron@gmail.com>
2024-09-06 03:12:26 +0100

    Re-org static content (#19)

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'restaurant/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'restaurant/static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 # Default primary key field type
