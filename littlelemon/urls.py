9b57a147b4f3b89df81ae7e32b727c92a0a1b1a7
Conor Heffron <conor.heffron@gmail.com>
2024-09-07 19:30:06 +0100

    Update urls.py files, refactor (#23)

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+from restaurant import views
 
 urlpatterns = [
     path('', include('restaurant.urls')),
-    path('/static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, })
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT, }),
+    re_path(r'.*', views.handler404)
 ]
