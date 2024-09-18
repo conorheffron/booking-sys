f8cdd4677c91d9f914d7ab2afae6cb5f14db410b
Conor Heffron <conor.heffron@gmail.com>
2024-09-19 00:05:16 +0100

    #29 Increase test coverage for views, handler (#45)
    * #29 Update Pipfile
    
    * #28 Update test.py
    
    - [x] Test cases for views
    - [x] Test case for 404 / page not found handler
    
    * #28 Update test.py
    
    - [x] Adding test case for invalid date format when calling bookings_by_date method

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -2,10 +2,11 @@
 Restaurant Tests Suite
 """
 from datetime import datetime
+import json
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from .models import Reservation
 from .forms import ReservationForm
-
 
 # Restaurant Tests
 class RestaurantTests(TestCase):
@@ -55,3 +56,117 @@ class RestaurantTests(TestCase):
         self.assertEqual(result.__dict__['data']["first_name"], "Conor")
         self.assertEqual(result.__dict__['data']["reservation_date"], booking_date)
         self.assertEqual(result.__dict__['data']["reservation_slot"], booking_slot)
+
+    def test_reservations_view_success(self):
+        """Restaurant Test case test_reservations_view_success
+        Parameters
+        ----------
+        self : TestCase
+        """
+        # when
+        response = self.client.get('/reservations/')
+
+        # then
+        self.assertContains(response, '<h3>All Bookings</h3>\n        ' +
+                            '<table>\n            <tr> \n' +
+                            '                <th>First Name</th>\n' +
+                            '                <th>Booking Date</th>\n' +
+                            '                <th>Booking time</th>\n' +
+                            '            </tr>\n            \n' +
+                            '        </table>\n        <br />\n        <button type="button" ' +
+                            'class="btn btn-primary" onClick="refresh()">Refresh</button>\n    ' +
+                            '</div>\n<body>', 
+                            status_code=200)
+        self.assertTemplateUsed(response, 'reservations.html')
+
+    def test_bookings_by_date_request_param_success(self):
+        """Restaurant Test case test_bookings_by_date_request_param_success
+        Parameters
+        ----------
+        self : TestCase
+        """
+        # when
+        response = self.client.get('/bookings?date=2024-09-18')
+
+        # then
+        self.assertContains(response, json.dumps({"message": "success", "reservations": []}),
+                            status_code=200)
+
+    def test_bookings_by_date_request_param_fail(self):
+        """Restaurant Test case test_bookings_by_date_request_param_fail
+        Parameters
+        ----------
+        self : TestCase
+        """
+        # when / then
+        with self.assertRaisesMessage(ValidationError,
+                                 '“2024-0918” value has an invalid date format. ' +
+                                 'It must be in YYYY-MM-DD format.'):
+            self.client.get('/bookings?date=2024-0918')
+
+    def test_bookings_by_date_path_var_success(self):
+        """Restaurant Test case test_bookings_by_date_path_var_success
+        Parameters
+        ----------
+        self : TestCase
+        """
+        # when
+        response = self.client.get('/bookings/2024-09-18/')
+
+        # then
+        self.assertContains(response, '', status_code=200)
+
+    def test_home_page_success(self):
+        """Restaurant Test case test_home_page_success
+        Parameters
+        ----------
+        self : TestCase
+        """
+        # when
+        response = self.client.get('/book/')
+
+        # then
+        self.assertContains(response, '\n        <h3>Bookings by Date</h3>\n        <table>\n' +
+                            '            <tbody id="tableData"></tbody>\n        </table>\n    ' +
+                            '</div>\n    \n</body>\n</html>', 
+                            status_code=200 )
+        self.assertTemplateUsed(response, 'booking.html')
+
+    def test_booking_success(self):
+        """Restaurant Test case test_booking_success
+        Parameters
+        ----------
+        self : TestCase
+        """
+        # given
+        test_name = 'Conor'
+        test_date = '2024-09-18'
+        test_time = '18:29'
+
+        # when
+        response = self.client.post('/book/', data={'first_name': test_name,
+                                                    'reservation_date': test_date,
+                                                    'reservation_slot': test_time})
+
+        # then
+        self.assertContains(response, json.dumps({"message": "Booking Complete",
+                                                  "reservations": [
+                                                      {"first_name": test_name,
+                                                       "reservation_date": test_date,
+                                                       "reservation_slot": test_time + ':00'}]}),
+                                                       status_code=200)
+
+    def test_handler404_success(self):
+        """Restaurant Test case test_handler404_success
+        Parameters
+        ----------
+        self : TestCase
+        """
+        # when
+        response = self.client.get('/reservations/invalid')
+
+        # then
+        self.assertContains(response, '<h1>Page Not Found: /reservations/invalid</h1>\n    ' +
+                            '</div>\n<body>', 
+                            status_code=404 )
+        self.assertTemplateUsed(response, 'error.html')
