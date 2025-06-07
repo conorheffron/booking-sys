8c515a8e7f159200ca2ed21fafff4092b303e1ba
Conor H <conor.heffron@gmail.com>
2025-06-07 14:30:59 +0100

    Update forms.py
    Restrict max length of name input for reservation form to 41 characters

--- hr/forms.py
+++ hr/forms.py
@@ -39,7 +39,7 @@ class ReservationForm(forms.Form):
     london_time = time_utils.get_current_date_time()
     # Time slots generated from 9:00 AM to 7:00 PM in 30-minute intervals
     TIME_SLOTS = time_utils.generate_time_slots(time(9, 0), time(19, 0), 30)
-    first_name = forms.CharField(max_length=200,
+    first_name = forms.CharField(max_length=41,
                                  min_length=3,
                                  widget=forms.widgets.TextInput
                                  (attrs={'placeholder': 'Enter Name...', 'class': 'form-control'}),
