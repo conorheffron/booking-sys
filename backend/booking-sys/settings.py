36368f35880f3f45521cc9f5b0f199b998721bee
Conor H <conor.heffron@gmail.com>
2025-06-12 01:39:26 +0100

    #49 Update settings.py

--- backend/booking-sys/settings.py
+++ backend/booking-sys/settings.py
@@ -27,13 +27,13 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 TEMPLATE_DEBUG = False
 
 if DEBUG is False:
-    ALLOWED_HOSTS = [ 'booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net', '169.254.129.4' ]
-    CSRF_TRUSTED_ORIGINS = [ 'https://booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net' ]
-    CORS_ALLOWED_ORIGINS = [ 'https://booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net' ]
-    CORS_ORIGIN_WHITELIST = [ 'https://booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net' ]
+    ALLOWED_HOSTS = [ '0.0.0.0:8000', '0.0.0.0', 'booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net', '169.254.129.4' ]
+    CSRF_TRUSTED_ORIGINS = [ 'https://booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net', 'https://169.254.129.4' ]
+    CORS_ALLOWED_ORIGINS = [ 'https://booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net', 'https://169.254.129.4' ]
+    CORS_ORIGIN_WHITELIST = [ 'https://booking-sys-ebgefrdmh3afbhee.northeurope-01.azurewebsites.net', 'https://169.254.129.4' ]
     CORS_ALLOW_ALL_ORIGINS = False
 else:
-    ALLOWED_HOSTS = [ 'localhost:8000', 'localhost' ]
+    ALLOWED_HOSTS = [ 'localhost:8000', '0.0.0.0', 'localhost' ]
     CORS_ALLOWED_ORIGINS = [ 'http://localhost:5173' ]
     CSRF_TRUSTED_ORIGINS = [ 'http://localhost:5173' ]
     CORS_ORIGIN_WHITELIST = [ 'http://localhost:5173' ]
