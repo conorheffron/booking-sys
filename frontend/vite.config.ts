fe55c5dcee1cdc39b7b2fd95ccfe462091dd9090
Conor Heffron <conor.heffron@gmail.com>
2025-06-13 01:03:39 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': 'http://0.0.0.0:8000',
       '/admin': 'http://0.0.0.0:8000',
