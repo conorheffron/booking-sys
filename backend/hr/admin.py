49129392dfdec7e8b39338993d02bc61d70c2678
Conor Heffron <conor.heffron@gmail.com>
2025-06-15 13:47:22 +0100

    Update admin.py

--- backend/hr/admin.py
+++ backend/hr/admin.py
@@ -5,9 +5,10 @@ from django.contrib import admin
 from .models import Reservation
 
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'reservation_date', 'reservation_slot')  # Fields to display in the list view
-    list_editable = ('first_name', 'reservation_date', 'reservation_slot')  # Fields to edit in the list view
-    search_fields = ('id', 'first_name')  # Add a search bar for these fields
-    list_filter = ('reservation_date', 'reservation_slot',)  # Add filters for these fields
+    """ReservationAdmin reservation table config for admin view"""
+    list_display = ('id', 'first_name', 'reservation_date', 'reservation_slot')
+    list_editable = ('first_name', 'reservation_date', 'reservation_slot')
+    search_fields = ('id', 'first_name')
+    list_filter = ('reservation_date', 'reservation_slot',)
 
 admin.site.register(Reservation, ReservationAdmin)
