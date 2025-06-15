1c31da5048027224d6445432dff4e100fbaf868c
Conor H <conor.heffron@gmail.com>
2025-06-15 13:44:24 +0100

    #167 Update admin.py

--- backend/hr/admin.py
+++ backend/hr/admin.py
@@ -1,5 +1,13 @@
 """
 Admin module
 """
+from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'reservation_date', 'reservation_slot')  # Fields to display in the list view
+    list_editable = ('first_name', 'reservation_date', 'reservation_slot')  # Fields to edit in the list view
+    search_fields = ('id', 'first_name')  # Add a search bar for these fields
+    list_filter = ('reservation_date', 'reservation_slot',)  # Add filters for these fields
+
+admin.site.register(Reservation, ReservationAdmin)
