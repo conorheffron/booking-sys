fec9f649d46c8f31380188deda66d0af1a14b5af
Conor H <conor.heffron@gmail.com>
2025-06-30 21:00:28 +0100

    #186 Update package-lock.json & vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     allowedHosts: true,
     proxy: {
+      '/api/docs/': 'http://0.0.0.0:8000',
       '/api': 'http://0.0.0.0:8000',
       '/admin': 'http://0.0.0.0:8000',
       '/static/admin': 'http://0.0.0.0:8000',
