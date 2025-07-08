f1d6efb4b25e84fcfae43e7fb8d53e1b3ef1bf73
Conor Heffron <conor.heffron@gmail.com>
2025-07-08 17:30:14 +0100

    Update settings.py

--- backend/booking-sys/settings.py
+++ backend/booking-sys/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1_-nbup&d1%klv)v$joa4oh4t+ikzmv0a@5asp%g&84=q#6p1m"
 SECRET_KEY = os.environ.get( "DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
